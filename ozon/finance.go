ac737ba415a8146559e706a2f4750638de530661
Anton Salnikov <antares@antares.me>
2023-10-26 14:14:36 +0400

    OperationType is an array of strings

--- ozon/finance.go
+++ ozon/finance.go
@@ -272,7 +272,7 @@ type ListTransactionsFilter struct {
 	Date ListTransactionsFilterDate `json:"date"`
 
 	// Operation type
-	OperationType string `json:"operation_type"`
+	OperationType []string `json:"operation_type"`
 
 	// Shipment number
 	PostingNumber string `json:"posting_number"`
