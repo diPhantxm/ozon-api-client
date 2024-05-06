ad2eb193255ec826142926abade31eb5476cdb7f
Kirill <asefru78@mail.ru>
2024-05-06 19:57:44 +0300

    Update May 2, 2024 (#89)

--- ozon/invoices.go
+++ ozon/invoices.go
@@ -16,11 +16,11 @@ type CreateUpdateProformaLinkParams struct {
 	// Shipment number
 	PostingNumber string `json:"posting_number"`
 
-	// Proforma invoice link
+	// Invoice link. Use the `v1/invoice/file/upload` method to create a link
 	URL string `json:"url"`
 
-	// Invoice HS-code. Pass a number up to 12 characters long
-	HSCode string `json:"hs_code"`
+	// Product HS-codes
+	HSCodes []CreateUpdateProformaLinkHSCode `json:"hs_codes"`
 
 	// Invoice date
 	Date time.Time `json:"date"`
@@ -28,11 +28,19 @@ type CreateUpdateProformaLinkParams struct {
 	// Invoice number. The number can contain letters and digits, maximum length is 50 characters
 	Number string `json:"number"`
 
-	// Cost stated in the invoice. The fractional part is separated by decimal point, up to two digits after the decimal poin
+	// Cost stated in the invoice. The fractional part is separated by decimal point, up to two digits after the decimal point
 	Price float64 `json:"price"`
 
 	// Invoice currency
 	PriceCurrency InvoiceCurrency `json:"price_currency" default:"USD"`
+}
+
+type CreateUpdateProformaLinkHSCode struct {
+	// Product HS code
+	Code string `json:"code"`
+
+	// Product identifier in the Ozon system, SKU
+	SKU string `json:"sku"`
 }
 
 type CreateUpdateProformaLinkResponse struct {
@@ -42,9 +50,9 @@ type CreateUpdateProformaLinkResponse struct {
 	Result bool `json:"result"`
 }
 
-// Create or edit proforma invoice link for VAT refund to Turkey sellers
+// Create or edit an invoice for VAT refund to Turkey sellers
 func (c Invoices) CreateUpdate(ctx context.Context, params *CreateUpdateProformaLinkParams) (*CreateUpdateProformaLinkResponse, error) {
-	url := "/v1/invoice/create-or-update"
+	url := "/v2/invoice/create-or-update"
 
 	resp := &CreateUpdateProformaLinkResponse{}
 
@@ -70,13 +78,32 @@ type GetProformaLinkResponse struct {
 }
 
 type GetProformaLinkResult struct {
-	// Proforma invoice link
+	// Invoice uploading date
+	Date time.Time `json:"date"`
+
+	// Invoice link
 	FileURL string `json:"file_url"`
+
+	// Product HS-codes
+	HSCodes []CreateUpdateProformaLinkHSCode `json:"hs_codes"`
+
+	// Invoice number
+	Number string `json:"number"`
+
+	// Cost stated in the invoice.
+	// The fractional part is separated by decimal point,
+	// up to two digits after the decimal point.
+	//
+	// Example: 199.99
+	Price float64 `json:"price"`
+
+	// Invoice currency
+	PriceCurrency InvoiceCurrency `json:"price_currency"`
 }
 
 // Get a proforma invoice link
 func (c Invoices) Get(ctx context.Context, params *GetProformaLinkParams) (*GetProformaLinkResponse, error) {
-	url := "/v1/invoice/get"
+	url := "/v2/invoice/get"
 
 	resp := &GetProformaLinkResponse{}
 
@@ -105,6 +132,36 @@ func (c Invoices) Delete(ctx context.Context, params *DeleteProformaLinkParams) 
 	url := "/v1/invoice/delete"
 
 	resp := &DeleteProformaLinkResponse{}
+
+	response, err := c.client.Request(ctx, http.MethodPost, url, params, resp, nil)
+	if err != nil {
+		return nil, err
+	}
+	response.CopyCommonResponse(&resp.CommonResponse)
+
+	return resp, nil
+}
+
+type UploadInvoiceParams struct {
+	// Base64 encoded invoice
+	Content string `json:"base64_content"`
+
+	// Shipment number
+	PostingNumber string `json:"posting_number"`
+}
+
+type UploadInvoiceResponse struct {
+	core.CommonResponse
+
+	// Link to invoice
+	URL string `json:"url"`
+}
+
+// Available file types: JPEG and PDF. Maximum file size: 10 MB
+func (c Invoices) Upload(ctx context.Context, params *UploadInvoiceParams) (*UploadInvoiceResponse, error) {
+	url := "/v1/invoice/file/upload"
+
+	resp := &UploadInvoiceResponse{}
 
 	response, err := c.client.Request(ctx, http.MethodPost, url, params, resp, nil)
 	if err != nil {
